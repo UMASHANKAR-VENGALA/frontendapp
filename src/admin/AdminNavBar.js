787c48c3bff128e5f392228807ec11136260224e
Umashankar Vengala <2200031870@kluniversity.in>
2024-04-23 12:32:34 +0530

    Admin localstorage

--- src/admin/AdminNavBar.js
+++ src/admin/AdminNavBar.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes, Link, useNavigate } from 'react-router-dom';
+import './admin.css';
+
+import AdminHome from './AdminHome';
+import ViewUsers from './ViewUsers';
+import ViewUserProfile from './ViewUserProfile';
 
 export default function AdminNavBar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('isAdminLoggedIn');
+    localStorage.removeItem('admin');
+    navigate('/adminlogin');
+    window.location.reload()
+  };
+
   return (
-    <div>AdminNavBar</div>
-  )
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/adminhome">Home</Link></li>
+          <li className="dropdown">
+            <Link>Users</Link>
+            <div className="dropdown-content">
+              <Link to="/viewusers">View Users</Link>
+            </div>
+          </li>
+          <li className="dropdown">
+            <Link>Songs</Link>
+            <div className="dropdown-content">
+                 <Link to="/createsong">Add Song</Link>
+                 <Link to="/viewsongs">View Songs</Link>
+            </div>
+          </li>
+          <li><button className="logoutButton" onClick={handleLogout}>Logout</button></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/adminhome" element={<AdminHome />} exact />
+        <Route path="/viewusers" element={<ViewUsers />} exact />
+        <Route path="/viewuserprofile/:email" element={<ViewUserProfile/>} exact />
+        
+      </Routes>
+    </div>
+  );
 }
