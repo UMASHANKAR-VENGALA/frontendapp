7415d56e08d463a2ffe2c6195823fe0a5501b57b
Hemanth Kalyan <2200032074@kluniversity.in>
2024-04-23 11:00:05 +0530

    login modification

--- src/admin/AdminLogin.js
+++ src/admin/AdminLogin.js
@@ -7,64 +7,61 @@ import { useNavigate } from 'react-router-dom';
 
 export default function AdminLogin() 
 {
-  const [formData, setFormData] = useState({
-    username: '',
-    password: ''
-  });
-  const [message,setMessage] = useState("")
-  const [error,setError] = useState("")
+  // const [formData, setFormData] = useState({
+  //   username: '',
+  //   password: ''
+  // });
+  // const [message,setMessage] = useState("")
+  // const [error,setError] = useState("")
 
-  // const navigate = useNavigate();
+  // // const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({ ...formData, [name]: value });
+  // };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try 
-    {
-      const response = await axios.post('http://localhost:2032/checkadminlogin', formData);
-      if (response.data!=null) 
-      {
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try 
+  //   {
+  //     const response = await axios.post('http://localhost:2032/checkadminlogin', formData);
+  //     if (response.data!=null) 
+  //     {
         // navigate("/adminhome")
-        window.location.href = "AdminSidebar"  
-      } 
-      else 
-      {
-        setMessage("Login Failed")
-        setError("")
-      }
-    } 
-    catch (error) 
-    {
-      setMessage("")
-      setError(error.message)
-    }
-  };
+  //     } 
+  //     else 
+  //     {
+  //       setMessage("Login Failed")
+  //       setError("")
+  //     }
+  //   } 
+  //   catch (error) 
+  //   {
+  //     setMessage("")
+  //     setError(error.message)
+  //   }
+  // };
 
   return (
     <div>
-
-      
-      <form onSubmit={handleSubmit}>
+      <form>
       <h3 align="center" style={{fontFamily:"cursive",fontSize:"40px"}}><u>Admin Login</u></h3>
       <br/>
-      {
+      {/* {
         message ? <h4 align="center">{message}</h4> : <h4 align="center">{error}</h4>
-      }
+      } */}
       <br/>
         <div>
           <label>Username</label>
-          <input type="text" name="username" value={formData.username} onChange={handleChange} placeholder='username' required />
+          <input type="text" name="username" placeholder='username' required />
         </div>
         <div>
           <label>Password</label>
-          <input type="password" name="password" value={formData.password} onChange={handleChange}placeholder='password' required />
+          <input type="password" name="password" placeholder='password' required />
         </div>
         <button type="submit" className="button-64" style={{fontWeight:"bolder"}}>Login</button>
-      </form>
+        </form>
     </div>
   );
 }
--- src/user/UserLogin.js
+++ src/user/UserLogin.js
@@ -1,7 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react';
+// import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
 
-export default function UserLogin() {
+
+export default function SignIn() 
+{
+  // const [formData, setFormData] = useState({
+  //   email: '',
+  //   password: ''
+  // });
+  // const [message,setMessage] = useState("")
+  // const [error,setError] = useState("")
+
+  // const handleChange = (e) => {
+  //   const { id, value } = e.target;
+  //   setFormData({ ...formData, [id]: value });
+  // };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try 
+  //   {
+  //     const response = await axios.post('http://localhost:2032/checkuserlogin', formData);
+  //     if (response.data!=null) 
+  //     {
+  //             console.log(response.data)
+  //             // navigate("/dashboard");    
+  //             window.location.href = "UserSidebar"    
+  //     } 
+  //     else 
+  //     {
+  //       setMessage("Login Failed")
+  //       setError("")
+  //     }
+  //   } 
+  //   catch (error) 
+  //   {
+  //     setMessage("")
+  //     setError(error.message)
+  //   }
+  // };
+
   return (
-    <div>UserLogin</div>
-  )
+    <div>
+      
+      <form>
+
+      <h2 align="center" style={{fontFamily:"cursive",fontSize:"40px"}}><u>Sign In</u></h2>
+      <br/>
+      {/* {
+        message ? <h4 align="center" style={{color:"red"}}>{message}</h4> : <h4 align="center" style={{color:"red"}}>{error}</h4>
+      } */}
+      <br/>
+        <div>
+          <label>Email</label>
+          <input type="email" id="email"  placeholder='Email' variant='standard' required />
+{/*           
+          <TextField type="email" id="email" label="Email" variant='standard' required /> */}
+        </div>
+        <br/>
+        <div>
+          <label>Password</label>
+          <input type="password" id="password" placeholder='Password' required />
+        </div>
+        <br/>
+        <button type="submit" className="button-64" style={{fontWeight:"bolder"}}>Login</button><br/>
+    
+        
+      </form>
+      
+      
+    </div>
+  );
 }
