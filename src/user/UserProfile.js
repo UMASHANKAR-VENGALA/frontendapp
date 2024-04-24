4ed08511337f3291a64449aec9e4f31a1a8163dc
Umashankar Vengala <2200031870@kluniversity.in>
2024-04-24 22:18:53 +0530

    User Profile page

--- src/user/UserProfile.js
+++ src/user/UserProfile.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './user.css';
 
 export default function UserProfile() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const storedUserData = localStorage.getItem('user');
+    if (storedUserData) {
+      const parsedUserData = JSON.parse(storedUserData);
+      setUserData(parsedUserData);
+    }
+  }, []);
+
   return (
-    <div>UserProfile</div>
-  )
+    userData ? (
+      <div className='profile-card'>
+        <p><strong>Full Name:</strong> {userData.fullname}</p>
+        <p><strong>Gender:</strong> {userData.gender}</p>
+        <p><strong>Email:</strong> {userData.email}</p>
+        <p><strong>Contact:</strong> {userData.contact}</p>
+      </div>
+    ) : (
+      <p>No User Data Found</p>
+    )
+  );
 }
