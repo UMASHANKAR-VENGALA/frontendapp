55caaa055efa7b5f1d7c537252a7894158cadcfd
Umashankar Vengala <2200031870@kluniversity.in>
2024-04-27 08:39:47 +0530

    get started

--- src/main/Home.js
+++ src/main/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css'; // Import your CSS file for styling
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
         <div align="center" className='cta-section'>
           <h2>Ready to dive into the world of music?</h2>
           <p>Start listening to your favorite tracks now!</p>
-          <button className='get-started-button'>Get Started</button>
+          <Link to="/userlogin"><button className='get-started-button' >Get Started</button></Link>
         </div>
       </div>
     </div>
